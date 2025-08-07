571d711fa788a11f7328ed89075fe3df5e9c93b7
Sivaji gadidala <93958018+sivajisj@users.noreply.github.com>
2025-08-07 12:00:31 +0000

    writing test cases

--- voting-dapp/anchor/tests/votingdapp.test.ts
+++ voting-dapp/anchor/tests/votingdapp.test.ts
@@ -1,3 +1,36 @@
 import * as anchor from '@coral-xyz/anchor';
 import { Program } from '@coral-xyz/anchor';
-import { PublicKey } from '@solana/web3.js';
+import { BankrunProvider, startAnchor } from "anchor-bankrun";
+
+import { Keypair, PublicKey } from '@solana/web3.js';
+import { describe, it } from 'node:test';
+import { Votingdapp } from "../target/types/votingdapp";
+import { publicKey } from '@coral-xyz/anchor/dist/cjs/utils';
+
+const IDL = require("../target/idl/votingdapp.json");
+
+const votingAddress = new PublicKey("JAVuBXeBZqXNtS73azhBDAoYaaAFfo4gWXoZe2e7Jf8H");
+
+describe("votingdapp", () => {
+    it("Intialize Poll", async () => {
+
+        const context = await startAnchor("", [{ name: "votingda", programId: votingAddress }], []);
+
+        const provider = new BankrunProvider(context);
+
+
+        const votingDappProgram = new Program<Votingdapp>(
+            IDL,
+            provider,
+        );
+        await votingDappProgram.methods.initializePoll(
+            new anchor.BN(1),
+            'this is voting dapp',
+            new anchor.BN(0),
+            new anchor.BN(1854495613)
+        ).rpc();
+        // const puppetKeypair = Keypair.generate();
+
+
+    })
+})
